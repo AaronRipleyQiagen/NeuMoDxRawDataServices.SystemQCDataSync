149f4cee910c07e00de043b9061a7d32c00af7fe
Aaron Ripley <RiplAMSC@qiagen.onmicrosoft.com>
2023-02-13 16:34:36 -0500

    updates to runreview module.

--- RunReview/pages/viewresults.py
+++ RunReview/pages/viewresults.py
@@ -8,25 +8,13 @@ register_page(__name__, path="/run-review/view-results/")
 
 fig = go.Figure()
 
-module_issues = [{'label': 'EP Variance is too high', 'value': 1},
-                 {'label': 'Ct Variance is too high', 'value': 2},
-                 {'label': 'Optics Baseline Left / Right Split', 'value': 3},
-                 {'label': 'Weak Overall Module Performance', 'value': 4}]
-
-lane_issues = [{'label': 'Inhibited Lane Performance', 'value': 1},
-               {'label': 'Step Noise Observed', 'value': 2},
-               {'label': 'Optics Noise Observed', 'value': 3},
-               {'label': 'Elevated Baseline', 'value': 4}
-               ]
-
 remediation_actions = [{'label': 'Increase Jack Pressure', 'value': 1},
                        {'label': 'Reflange Fluid Line', 'value': 2},
                        {'label': 'Perform Optics Calibration', 'value': 3},
                        {'label': 'Repeat Testing As Is', 'value': 4}]
 
 run_review_description = html.H1(id='run-review-description')
-run_review_channel_selector = dcc.Dropdown(
-    ['Yellow', 'Green', 'Orange', 'Red', 'Far Red'], value='Yellow', id='run-review-channel-selector')
+run_review_channel_selector = dcc.Dropdown(id='run-review-channel-selector')
 run_review_process_step_selector = dcc.Dropdown(
     ['Normalized', 'Raw', '2nd'], value='Normalized', id='run-review-process-step-selector')
 run_review_curves = dcc.Graph(id="run-review-curves", figure=fig)
@@ -34,7 +22,7 @@ run_review_line_data = dash_table.DataTable(
     id='runset-sample-results', row_selectable='Multi', sort_action='native')
 
 
-tab1_content = dbc.Card(
+line_data_content = dbc.Card(
     dbc.CardBody(
         [
             run_review_line_data
@@ -43,29 +31,74 @@ tab1_content = dbc.Card(
     className="mt-3",
 )
 
-tab2_content = dbc.Card(
+module_issue_content = dbc.Card(
     dbc.CardBody(
         [
-            html.P("Please Select Issue", className="card-text"),
-            dcc.Dropdown(id='module-issue-options', options=module_issues),
-            dbc.Button("Submit Module Issue", id='module-issue-submit')
+            html.Div([html.P("What type of issue is this?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='module-issue-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("What optics channel is this issue observed on?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='module-issue-channel-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("How severe is this issue?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='module-issue-severity-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            dbc.Button('Submit Module Issue', id='submit-sample-issue')
         ]
     ),
     className="mt-3",
 )
 
-tab3_content = dbc.Card(
+
+run_issue_content = dbc.Card(
     dbc.CardBody(
         [
-            html.P("Please Select Issue", className="card-text"),
-            dcc.Dropdown(id='lane-issue-options', options=lane_issues),
-            dbc.Button("Submit Lane Issue", id='lane-issue-submit')
+            html.Div([html.P("Which run was issue observed in?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='run-issue-run-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("What type of issue is this?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='run-issue-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("What optics channel is this issue observed on?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='run-issue-channel-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("How severe is this issue?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='run-issue-severity-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            dbc.Button('Submit Run Issue', id='submit-sample-issue')
+        ]
+    )
+)
+
+module_lane_issue_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.Div([html.P("What lane is affected by issue?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='lane-issue-lane-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("What type of issue is this?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='lane-issue-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("What optics channel is this issue observed on?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='lane-issue-channel-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("How severe is this issue?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='lane-issue-severity-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            dbc.Button('Submit Lane Issue', id='submit-sample-issue')
         ]
     ),
     className="mt-3",
 )
 
-tab4_content = dbc.Card(
+sample_issue_content = dbc.Card(
+    dbc.CardBody(
+        [
+            html.Div([html.P("Which run was issue observed in?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='sample-issue-run-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("What lane is affected by issue?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='sample-issue-lane-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("What type of issue is this?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='sample-issue-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("What optics channel is this issue observed on?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='sample-issue-channel-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            html.Div([html.P("How severe is this issue?", className="card-text", style={'width': '30%', 'display': 'inline-block', 'vertical-align': 'middle'}),
+                      dcc.Dropdown(id='sample-issue-severity-options', style={'width': '70%', 'display': 'inline-block', 'vertical-align': 'middle'})]),
+            dbc.Button('Submit Sample Issue', id='submit-sample-issue')
+        ]
+    )
+)
+
+active_issues_content = dbc.Card(
     dbc.CardBody(
         [
             html.P("Coming Soon", className="card-text"),
@@ -74,7 +107,7 @@ tab4_content = dbc.Card(
     className="mt-3",
 )
 
-tab5_content = dbc.Card(
+remediation_action_content = dbc.Card(
     dbc.CardBody(
         [
             html.P("Please Choose a Remediation Action", className="card-text"),
@@ -88,11 +121,13 @@ tab5_content = dbc.Card(
 
 tabs = dbc.Tabs(
     [
-        dbc.Tab(tab1_content, label="View Line Data"),
-        dbc.Tab(tab2_content, label="Note Module Issue"),
-        dbc.Tab(tab3_content, label="Note Lane Issue"),
-        dbc.Tab(tab4_content, label='View Active Issues'),
-        dbc.Tab(tab5_content, label='Assign Remediation Action')
+        dbc.Tab(line_data_content, label="View Line Data"),
+        dbc.Tab(module_issue_content, label="Note Module Issue"),
+        dbc.Tab(run_issue_content, label="Note Run Issue"),
+        dbc.Tab(module_lane_issue_content, label="Note Lane Issue"),
+        dbc.Tab(sample_issue_content, label="Note Sample Issue"),
+        dbc.Tab(active_issues_content, label='View Active Issues'),
+        dbc.Tab(remediation_action_content, label='Assign Remediation Action')
     ]
 )
 layout = [dcc.Loading(id='run-review-loading', type='graph', children=[
--- RunReview/run_review.py
+++ RunReview/run_review.py
@@ -1,4 +1,5 @@
-from dash import Input, Output, dcc, html, no_update
+from flask import session
+from dash import Input, Output, dcc, html, no_update, ctx
 import dash_bootstrap_components as dbc
 from dash import Dash
 from .appbuildhelpers import apply_layout_with_auth
@@ -11,6 +12,8 @@ import pandas as pd
 import numpy as np
 import warnings
 import plotly.graph_objects as go
+import aiohttp
+import asyncio
 
 warnings.filterwarnings('ignore')
 
@@ -74,8 +77,14 @@ content = html.Div(id="run-reviewer-page-content", style=CONTENT_STYLE,
 runset_selection = dcc.Store(
     id='runset-selection-data', storage_type='session')
 runset_sample_data = dcc.Store(id='runset-sample-data', storage_type='session')
-layout = html.Div([review_loader, dcc.Location(
-    id="run-review-url", refresh=True), sidebar, content, runset_selection, runset_sample_data])
+runset_review_id = dcc.Store(id='runset-review-id', storage_type='session')
+runset_channel_options = dcc.Store(
+    id='runset-channel-options', storage_type='session')
+runset_severity_options = dcc.Store(
+    id='runset-severity-options', storage_type='session')
+channel_selected = dcc.Store(id='channel-selected')
+layout = html.Div([review_loader, dcc.Loading(id='run-review-href-loader', fullscreen=True, type='dot', children=[dcc.Location(
+    id="run-review-url", refresh=True)]), sidebar, content, runset_selection, runset_sample_data, runset_review_id, runset_severity_options, runset_channel_options, channel_selected])
 
 
 def Add_Dash(app):
@@ -101,15 +110,22 @@ def Add_Dash(app):
         print(runsetsample_url)
         _runset_samples = requests.get(runsetsample_url, verify=False).json()
         return _runset_samples
-        #    Output('run-review-channel-selector', 'options'),
-        #    Output('run-review-process-step-selector', 'options')],
 
-    @app.callback(Output("runset-sample-data", "data"),
+    @app.callback([Output("runset-sample-data", "data"),
+                   Output("run-review-url", "href"),
+                   Output('runset-review-id', 'data'),
+                   Output('runset-severity-options', 'data'),
+                   Output('runset-channel-options', 'data')],
                   [Input('get-runset-data', 'n_clicks'),
                    State('runset-selection-data', 'data')],
                   prevent_inital_call=True)
     def get_runset_samples(n, runset_data):
-        print("trying...")
+
+        if n == 0:
+            return no_update
+        """
+        Get Data associated with Runset.
+        """
         runset_sample_ids = []
         runset_map_df = pd.DataFrame(columns=['RawDataDatabaseId', 'RunSetXPCRModuleLaneId',
                                               'RunSetCartridgeId', 'RunSetXPCRModuleId', 'RunSetNeuMoDxSystemId'])
@@ -137,16 +153,103 @@ def Add_Dash(app):
             'Far_Red', 'Far Red')
         dataframe = dataframe.set_index(
             'RawDataDatabaseId').join(runset_map_df).reset_index()
-        # dataframe.to_csv('test-merged.csv')
-        # print(runset_data['name'] + " " + str(runset_data['number']))
-        return dataframe.to_dict('records')
+
+        """
+        Get or Add RunSet Review
+        """
+
+        runsetreview = {}
+        runsetreview['userId'] = session['user'].id
+        runsetreview['reviewerName'] = session['user'].display_name
+        runsetreview['reviewerEmail'] = session['user'].emails[0]
+        runsetreview['runSetId'] = runset_data['id']
+        resp = requests.post(url=os.environ['RUN_REVIEW_API_BASE'] +
+                             "RunSetReviews", json=runsetreview, verify=False).json()
+
+        """
+        Get Severity Options
+        """
+        severity_url = os.environ['RUN_REVIEW_API_BASE'] + "SeverityRatings"
+        severity_resp = requests.get(severity_url, verify=False).json()
+        severity_options = []
+        for severity in severity_resp:
+            severity_options.append(
+                {'label': severity['name'], 'value': severity['id']})
+
+        """
+        Get Run Set Channel options
+        """
+        channels_data = runset_data['runSetType']['qualificationAssay']['assayChannels']
+        channel_options = {}
+        for channel in channels_data:
+            channel_options[channel['id']] = channel['channel']
+
+        """
+        Return Data associated with Runset, URL for RunsetReview Page, Runset Review Id, Severity Options.
+        """
+
+        return dataframe.to_dict('records'), '/dashboard/run-review/view-results', resp['id'], severity_options, channel_options
+
+    @app.callback([Output('sample-issue-options', 'options'), Output('lane-issue-options', 'options'), Output('module-issue-options', 'options')],
+                  Input('submit-sample-issue', 'children'))
+    def getIssueTypes(_):
+
+        async def getIssueTypeOptions(session, url):
+            async with session.get(url) as resp:
+                issueTypes = await resp.json()
+                return issueTypes
+
+        async def main():
+
+            async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+                issueTypeEndpoints = [
+                    'SampleIssueTypes', 'XPCRModuleLaneIssueTypes', 'CartridgeIssueTypes', 'XPCRModuleIssueTypes']
+                tasks = []
+                tasksTracker = {}
+                for issueType in issueTypeEndpoints:
+                    url = os.environ['RUN_REVIEW_API_BASE'] + issueType
+                    tasks.append(asyncio.ensure_future(
+                        getIssueTypeOptions(session, url)))
+
+                responses = await asyncio.gather(*tasks)
+
+                return {issueTypeEndpoints[i]: responses[i] for i in range(len(responses))}
+
+        issueTypes = asyncio.run(main())
+
+        issueTypeOptions = {}
+        issueTypeEndpoints = ['SampleIssueTypes', 'XPCRModuleLaneIssueTypes',
+                              'CartridgeIssueTypes', 'XPCRModuleIssueTypes']
+        for endpoint in issueTypeEndpoints:
+            issue_type_options = []
+            for issueType in issueTypes[endpoint]:
+                issue_type_options.append(
+                    {'label': issueType['name'], 'value': issueType['id']})
+            issueTypeOptions[endpoint] = issue_type_options
+
+        return issueTypeOptions['SampleIssueTypes'], issueTypeOptions['XPCRModuleLaneIssueTypes'], issueTypeOptions['XPCRModuleIssueTypes']
+
+    @app.callback([Output('sample-issue-severity-options', 'options'),
+                   Output('lane-issue-severity-options', 'options'),
+                   Output('run-issue-severity-options', 'options'),
+                   Output('module-issue-severity-options', 'options')
+                   ],
+                  Input('runset-severity-options', 'data'))
+    def update_severity_options(data):
+        return data, data, data, data
 
     @app.callback([Output('run-review-description', 'children'), Output('run-review-curves', 'figure'), Output('runset-sample-results', 'data')],
-                  [Input('run-review-channel-selector', 'value'),
+                  [Input('channel-selected', 'data'),
                    Input('run-review-process-step-selector', 'value'),
-                   Input('runset-sample-data', 'data'),
-                   State('runset-selection-data', 'data')], prevent_initial_call=True)
-    def update_pcr_curves(channel, process_step, data, runset_data):
+                   State('runset-sample-data', 'data'),
+                   State('runset-selection-data', 'data'),
+                   State('runset-channel-options', 'data')], prevent_initial_call=True)
+    def update_pcr_curves(channel_selected, process_step, data, runset_data, channel_options):
+        if channel_selected == None:
+            channel = 'Yellow'
+        else:
+            channel = channel_options[channel_selected]
+
         dataframe = pd.DataFrame.from_dict(data)
         dataframe['Channel'] = dataframe['Channel'].replace(
             'Far_Red', 'Far Red')
@@ -172,6 +275,44 @@ def Add_Dash(app):
                                      line=dict(color=colorDict[df_Channel_Step.loc[idx, 'XPCR Module Lane']])))
 
         return [runset_data['name'] + " Attempt # " + str(runset_data['number'])], fig, df_Channel_Step[['XPCR Module Serial', 'XPCR Module Lane', 'Sample ID', 'Target Name', 'Localized Result', 'Overall Result', 'Ct', 'End Point Fluorescence', 'Max Peak Height', 'EPR']].round(1).to_dict('records')
+
+    @app.callback([Output('sample-issue-channel-options', 'options'),
+                   Output('lane-issue-channel-options', 'options'),
+                   Output('run-issue-channel-options', 'options'),
+                   Output('module-issue-channel-options', 'options'),
+                   Output('run-review-channel-selector', 'options')],
+                  Input('runset-channel-options', 'data'))
+    def update_channel_options(data):
+        return data, data, data, data, data
+
+    @app.callback(Output('channel-selected', 'data'),
+                  [Input('sample-issue-channel-options', 'value'),
+                   Input('lane-issue-channel-options', 'value'),
+                   Input('run-issue-channel-options', 'value'),
+                   Input('module-issue-channel-options', 'value'),
+                   Input('run-review-channel-selector', 'value')], prevent_initial_call=True
+                  )
+    def update_channel_selection(sample_issue_channel, lane_issue_channel, run_issue_channel, module_issue_channel, run_review_channel):
+        channel_adjusted = ctx.triggered_id
+        if channel_adjusted == 'sample-issue-channel-options':
+            return sample_issue_channel
+        elif channel_adjusted == 'lane-issue-channel-options':
+            return lane_issue_channel
+        elif channel_adjusted == 'run-issue-channel-options':
+            return run_issue_channel
+        elif channel_adjusted == 'module-issue-channel-options':
+            return module_issue_channel
+        elif channel_adjusted == 'run-review-channel-selector':
+            return run_review_channel
+
+    @app.callback([Output('sample-issue-channel-options', 'value'),
+                   Output('lane-issue-channel-options', 'value'),
+                   Output('run-issue-channel-options', 'value'),
+                   Output('module-issue-channel-options', 'value'),
+                   Output('run-review-channel-selector', 'value')],
+                  Input('channel-selected', 'data'), prevent_initial_call=True)
+    def update_channel_selection_value(channel):
+        return channel, channel, channel, channel, channel
     return app.server
 
 
@@ -182,56 +323,3 @@ if __name__ == '__main__':
     app.layout = html.Div([review_loader, dcc.Location(
         id="run-review-url"), sidebar, content])
     app.run_server(debug=True)
-
-# Start Test
-
-# from dash import Dash
-# # from .appbuildhelpers import apply_layout_with_auth
-# from dash import html, callback, Output, Input, State, register_page, dcc, dash_table, page_container
-
-# import dash_bootstrap_components as dbc
-# from dash import Input, Output, dcc, html
-
-
-# # the style arguments for the sidebar. We use position:fixed and a fixed width
-# SIDEBAR_STYLE = {
-#     "position": "fixed",
-#     "top": 0,
-#     "left": 0,
-#     "bottom": 0,
-#     "width": "16rem",
-#     "padding": "2rem 1rem",
-#     "background-color": "#f8f9fa",
-# }
-
-# # the styles for the main content position it to the right of the sidebar and
-# # add some padding.
-# CONTENT_STYLE = {
-#     "margin-left": "18rem",
-#     "margin-right": "2rem",
-#     "padding": "2rem 1rem",
-# }
-
-# sidebar = html.Div(
-#     [
-#         html.H3("Run Review Options", className="display-6"),
-#         html.Hr(),
-#         dbc.Nav(
-#             [
-#                 dbc.NavLink("Home", href="/dashboard/run-review/",
-#                             active="exact"),
-#                 dbc.NavLink("Run Review Queue",
-#                             href="/dashboard/run-review/review-queue", active="exact"),
-#                 dbc.NavLink("View Module History",
-#                             href="/dashboard/run-review/module-history", active="exact"),
-#             ],
-#             vertical=True,
-#             pills=True,
-#         ),
-#     ],
-#     style=SIDEBAR_STYLE,
-# )
-
-# review_loader = html.Div(id='run-reviewer-loader')
-# content = html.Div(id="run-reviewer-page-content", style=CONTENT_STYLE,
-#                    children=page_container)
