0b0c294702a489ab423fae1dbc44bce625b926cd
Aaron Ripley <RiplAMSC@qiagen.onmicrosoft.com>
2023-06-30 06:53:15 -0400

    cleaned up populate_review_queue

--- RunReviewQueue/functions.py
+++ RunReviewQueue/functions.py
@@ -13,92 +13,78 @@ from Shared.functions import *
 
 
 def populate_review_queue(
-    user_id,
-    user_group,
-    review_group_ids=[],
-    runset_statuses=[],
-    reviewer_group_id=None,
-    my_runsets_filter=False,
+    user_id: str,
+    user_group_id: str,
+    review_group_ids: list = [],
+    runset_status_ids: list = [],
+    my_runsets_filter: bool = False,
 ):
+    """
+    A function used to retrieve & process information necessary to populate the run review queue.
+
+    Args:
+        user_id: The id of the user of interest.
+        user_group_id: the id of the review group associated with the user of interest.
+        reviewer_group_ids: The ids of the review groups to filter the runset review assignments to (Defaults to None).
+        runset_status_ids: The ids of the of the runset statuses to filter the runset review assignments to (Defaults to None).
+        my_runsets_filter: Whether or not to filter the runsets to results that were created by the user of interest (Defaults to False).
+
+    """
+
+    ## Construct endpoint url by passing user_id into template string.
     runsets_url = os.environ[
         "RUN_REVIEW_API_BASE"
     ] + "RunSets/{}/reviewerstatus".format(user_id)
+
+    ## Create a dictionary that captures the query_params.
     runsets_params = {}
     if len(review_group_ids) > 0:
         runsets_params["reviewgroupids"] = review_group_ids
-    if len(runset_statuses) > 0:
-        runsets_params["runsetstatuses"] = runset_statuses
-    if reviewer_group_id:
-        runsets_params["reviewergroupid"] = reviewer_group_id
+    if len(runset_status_ids) > 0:
+        runsets_params["runsetstatuses"] = runset_status_ids
+    if user_group_id:
+        runsets_params["reviewergroupid"] = user_group_id
     runsets_params["createdbyuserfilter"] = my_runsets_filter
+
+    ## Make request
     resp = requests.get(url=runsets_url, verify=False, params=runsets_params)
+
+    ## Process Request
     runsets = resp.json()
-    if len(runsets) > 0:
-        df = pd.DataFrame.from_dict(runsets)
-        df["Status"] = np.nan
 
-        for idx in df.index:
-            try:
-                df.loc[idx, "Status"] = df.loc[idx, "runSetReviews"][0][
-                    "runSetReviewStatus"
-                ]["name"]
-            except:
-                df.loc[idx, "Status"] = "Not Yet Reviewed"
+    ## Get User Names for unique user_ids
+    unique_user_ids = list(set(map(lambda item: item.get("validFromUser"), runsets)))
+    users = get_users_info_async(unique_user_ids)
+    user_names = dict([(user, users[user]["displayName"]) for user in users])
 
-        df["XPCR Module"] = [
-            x[0]["xpcrModule"]["xpcrModuleSerial"] for x in df["runSetXPCRModules"]
-        ]
-
-        columns = [
-            "id",
-            "Status",
-            "XPCR Module",
-            "name",
-            "runSetStartDate",
-            "sampleCount",
-            "validFromUser",
-            "validFrom",
-            "Created By",
-        ]
-
-        groupable_columns = ["Status"]
-
-        """
-        Get User Names
-        """
-
-        users = get_users_info_async([x for x in df["validFromUser"].unique()])
-
-        user_info = {}
-        for user in users:
-            user_info[user] = users[user]["givenName"]
-
-        df["Created By"] = df["validFromUser"].replace(user_info)
-
-        column_names = {
-            "name": "Description",
-            "runSetStartDate": "Start Date",
-            "sampleCount": "Sample Count",
-            "validFromUser": "UserId",
-            "id": "Id",
-            "validFrom": "Created Date",
+    runsets = [
+        {
+            **record,
+            "XPCR Module": record["runSetXPCRModules"][0]["xpcrModule"][
+                "xpcrModuleSerial"
+            ],
+            "My Status": record["runSetReviews"][0]["runSetReviewStatus"]["name"]
+            if len(record["runSetReviews"]) > 0
+            else "Not Yet Reviewed",
+            "Overall Status": record["runSetStatus"]["name"],
+            "Created By": user_names[record["validFromUser"]],
         }
+        for record in runsets
+    ]
 
-        df = df[columns].rename(column_names, axis=1)
+    column_map = {
+        "id": "Id",
+        "My Status": "My Status",
+        "Overall Status": "Overall Status",
+        "XPCR Module": "XPCR Module",
+        "name": "Description",
+        "Created By": "Created By",
+        "runSetStartDate": "Start Date",
+        "validFrom": "Created Date",
+    }
 
-        columnDefs = []
-        for column in df.columns:
-            columnDef = {"headerName": column, "field": column, "filter": True}
-            if "Date" in column:
-                df[column] = (
-                    df[column].astype("datetime64").dt.strftime("%d %B %Y %H:%M:%S")
-                )
-            if column in groupable_columns:
-                columnDef["rowGroup"] = True
-            if "Id" in column:
-                columnDef["hide"] = True
-            columnDefs.append(columnDef)
-        return df.sort_values(["XPCR Module"]).to_dict("records"), columnDefs
+    groupable_columns = ["My Status"]
 
-    else:
-        return None, None
+    return get_dash_ag_grid_from_records(
+        records=runsets, column_map=column_map, group_columns=groupable_columns
+    )
