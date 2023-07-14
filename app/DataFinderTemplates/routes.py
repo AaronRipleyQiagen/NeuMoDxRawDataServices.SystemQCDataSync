941c114c7d98e193f7713d65d7e4aeed19d36f36
Aaron Ripley <RiplAMSC@qiagen.onmicrosoft.com>
2023-07-14 13:05:18 -0400

    add ability to parse querystring datafinder route

--- app/DataFinderTemplates/routes.py
+++ app/DataFinderTemplates/routes.py
@@ -2,9 +2,14 @@ from . import blueprint
 from flask import render_template, request
 from app.webapp import login_required
 from DataFinder import data_finder
+from urllib.parse import urlencode
 
 
 @blueprint.route("/", methods=["GET"])
 @login_required
 def datafinder():
-    return render_template("data-finder.html", dash_url=data_finder.url_base)
+    xpcr_module_ids = request.args.getlist("xpcrmoduleid")
+    query_string = urlencode({"xpcrmoduleid": xpcr_module_ids}, doseq=True)
+    return render_template(
+        "data-finder.html", dash_url=data_finder.url_base + "?" + query_string
+    )
