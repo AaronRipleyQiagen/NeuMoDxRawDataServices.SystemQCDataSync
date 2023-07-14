489780d902d434e92064e13ab37ff705bb8ed7a0
Aaron Ripley <RiplAMSC@qiagen.onmicrosoft.com>
2023-07-14 13:05:50 -0400

    updated querystring labels for datareviewer

--- app/DataReviewerTemplates/routes.py
+++ app/DataReviewerTemplates/routes.py
@@ -8,8 +8,8 @@ from DataReviewer import data_reviewer
 @blueprint.route("/", methods=["GET"])
 @login_required
 def runreview():
-    runset_ids = request.args.getlist("runset_ids")
-    query_string = urlencode({"my_list": runset_ids}, doseq=True)
+    cartridge_ids = request.args.getlist("cartridgeid")
+    query_string = urlencode({"cartridgeid": cartridge_ids}, doseq=True)
     return render_template(
         "data-reviewer.html", dash_url=data_reviewer.url_base + "?" + query_string
     )
